85a4d4353ec663f7d42c39ce364d1c84d745583c
Arsh234-GIT <ahamadarsh92@gmail.com>
2023-07-15 03:30:01 +0530

    iitial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         return (
             <header className="header">
                 <div className="left-container">
-                <img className="troll-face" src="../Images/Troll Face.png"/>
+                <img className="troll-face" src="../Meme-Generator/Images/Troll Face.png"/>
                 <p className="left-heading ">Meme Generator</p> 
                 </div>
                 
